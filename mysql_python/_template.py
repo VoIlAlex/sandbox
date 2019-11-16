500e5520d1b5b3323092f936cd03fb209a9b7348
Ilya <ilya.vouk@gmail.com>
2019-11-16 19:11:39 +0300

    Template for mysql scripts

---
+++ mysql_python/_template.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+
+def check_requirements(cursor):
+    pass
+
+
+if __name__ == "__main__":
+    # connect to the server
+    db = mysql.connector.connect(
+        host='localhost',
+        user='voilalex',
+        passwd='VoIlAlex_VoIlAlex_2000'
+    )
+    cursor = db.cursor()
+    check_requirements(cursor)
+
+    ##########
+    ## MAIN ##
+    ##########
